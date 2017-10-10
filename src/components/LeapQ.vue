cbfd7c3bf3aaef0db15488ac1a0e5f2abe9c6c23
Tychio <code@tychio.net>
2017-10-10 21:35:46 +0800

    add info form

--- src/components/LeapQ.vue
+++ src/components/LeapQ.vue
@@ -1,19 +1,62 @@
 <template>
   <main>
-    <form>
-        
-    </form>
+    <info-form :info="info" v-if="step === 1"></info-form>
+
+    <ButtonGroup shape="circle" size="large">
+      <Button @click="prev" type="default">
+        <Icon type="chevron-left"></Icon>
+        <span>上一步</span>
+      </Button>
+      <Button @click="next" type="success">
+        <span>下一步</span>
+        <Icon type="chevron-right"></Icon>
+      </Button>
+    </ButtonGroup>
   </main>
 </template>
 
 <script>
+import InfoForm from './InfoForm'
+
 export default {
   name: 'LeapQ',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      step: 1,
+      info: {
+        firstname: '',
+        lastname: '',
+        birthday: new Date('2000-01-01'),
+        age: 20,
+        gender: 0,
+        university: '',
+        college: '',
+        major: '',
+        studentnumber: '',
+        phone: '',
+        qq: '',
+        wechat: ''
+      }
     }
+  },
+  methods: {
+    next: function () {
+      this.step++
+    },
+    prev: function () {
+      this.step = (this.step - 1) || 1
+    }
+  },
+  components: {
+    'info-form': InfoForm
   }
 }
 </script>
+
+<style>
+main {
+  width: 840px;
+  margin: auto;
+}
+</style>
 

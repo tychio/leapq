b5755d8acc6134afda54f090b8d46a763ea49fdd
Tychio <code@tychio.net>
2017-10-10 20:32:59 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <header>
+      <p>改编自<strong>西北双语和心理语言研究实验室</strong></p>
+      <p>玛丽安，布鲁莫菲尔德，及考山斯卡娅2007版的LEAP-Q</p>
+      <p>改编者 吴蕊霖 布鲁塞尔自由大学</p>
+      <h1>语言经历和语言水平调查问卷 (Leap-Q)</h1>
+    </header>
+    <leapq/>
+    <footer>
+      <ul>
+        <li><a href="http://www.vub.ac.be/" target="_blank">Vrije Universiteit Brussel</a></li>
+      </ul>
+    </footer>
+  </div>
+</template>
+
+<script>
+import LeapQ from './components/LeapQ'
+
+export default {
+  name: 'app',
+  components: {
+    leapq: LeapQ
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/LeapQ.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <form>
+        
+    </form>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'LeapQ',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+

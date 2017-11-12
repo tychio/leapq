cb5e1b405a67a4ae2a5404c969232e72d4534ac8
Tychio <code@tychio.net>
2017-11-12 15:49:50 +0800

    adjust header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
     <header>
-      <p>改编自<strong>西北双语和心理语言研究实验室</strong></p>
-      <p>玛丽安，布鲁莫菲尔德，及考山斯卡娅2007版的LEAP-Q</p>
-      <p>改编者 吴蕊霖 布鲁塞尔自由大学</p>
       <h1>语言经历和语言水平调查问卷 (Leap-Q)</h1>
     </header>
     <leapq/>
@@ -11,6 +8,9 @@
       <ul>
         <li><a href="http://www.vub.ac.be/" target="_blank">Vrije Universiteit Brussel</a></li>
       </ul>
+      <p>改编自<strong>西北双语和心理语言研究实验室</strong></p>
+      <p>玛丽安，布鲁莫菲尔德，及考山斯卡娅2007版的LEAP-Q</p>
+      <p>改编者 吴蕊霖 布鲁塞尔自由大学</p>
     </footer>
   </div>
 </template>

8ff2bdcb993f0bfbb824a4605d3f65fb5d9494ad
Tychio <code@tychio.net>
2020-11-19 15:33:54 +0100

    test sentry

--- src/main.js
+++ src/main.js
@@ -5,6 +5,23 @@ import iView from 'iview'
 import 'iview/dist/styles/iview.css'
 import App from './App'
 import VueTouch from 'vue-touch'
+import * as Sentry from '@sentry/browser'
+import { Vue as VueIntegration } from '@sentry/integrations'
+import { Integrations } from '@sentry/tracing'
+
+Sentry.init({
+  dsn: 'https://2784769aa82e450fb3944e49a332cba7@o479270.ingest.sentry.io/5523645',
+  integrations: [
+    new VueIntegration({
+      Vue,
+      tracing: true
+    }),
+    new Integrations.BrowserTracing()
+  ],
+
+  tracesSampleRate: 1.0
+})
+
 Vue.use(VueTouch, {name: 'v-touch'})
 Vue.use(iView)
 Vue.config.productionTip = false
